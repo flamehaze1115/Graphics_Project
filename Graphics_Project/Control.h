5fbc7132289c88309feded778ff52d2835156a4d
Flamehaze <longxiao1115@qq.com>
2016-12-03 16:23:09 +0800

    can display skybox,only

---
+++ Graphics_Project/Control.h
@@ -0,0 +1,8 @@
+#pragma once
+#include"stdafx.h"
+#define ZeroMemory RtlZeroMemory
+/** ¶¨Òå¼üÅÌÀà */
+void key(unsigned char k, int x, int y)
+{
+
+}
--- Graphics_Project/main.cpp
+++ Graphics_Project/main.cpp
@@ -2,21 +2,68 @@
 //
 
 #include <glut.h>
+#include"BMPLoader.h"
+#include"Camera.h"
+#include"SkyBox.h"
+#include"stdafx.h"
+#include"Vector.h"
+#include"Control.h"
+
+bool    m_RenderMode;		          /**< 绘制模式 */
+CSkyBox m_SkyBox;
+Camera m_Camera;
 
 float fTranslate;
 float fRotate;
 float fScale = 1.0f;	// set inital scale value to 1.0f
+float fDistance = 0.2f;
 
-void Draw_Triangle() // This function draws a triangle with RGB colors
+bool bPersp = false;
+bool bAnim = false;
+bool bWire = false;
+
+int wHeight = 0;
+int wWidth = 0;
+
+bool init() {
+	/** 用户自定义的初始化过程 */
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+	glClearDepth(1.0f);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	/** 初始化天空 */
+	if (!m_SkyBox.init())
+	{
+		MessageBox(NULL, L"初始化天空失败!", L"错误", MB_OK);
+		exit(0);
+	}
+
+	/** 设置摄像机 */
+	m_Camera.setCamera(0, 0, 8, 0, 0, 0, 0, 1, 0);
+
+	return true;                                        /**< 成功返回 */
+}
+
+
+
+void updateView(int width, int height)
 {
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-1.0f, -1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(1.0f, -1.0f, 0.0f);
-	glEnd();
+
+	glViewport(0, 0, width, height);						// Reset The Current Viewport
+
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glLoadIdentity();									// Reset The Projection Matrix
+
+	float whRatio = (GLfloat)width / (GLfloat)height;
+
+	gluPerspective(45, 1, 1, 100);
+	
+
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
 }
 
 void reshape(int width, int height)
@@ -26,16 +73,10 @@ void reshape(int width, int height)
 		height = 1;										// Making Height Equal One
 	}
 
-	glViewport(0, 0, width, height);						// Reset The Current Viewport
+	wHeight = height;
+	wWidth = width;
 
-	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
-	glLoadIdentity();									// Reset The Projection Matrix
-
-														// Calculate The Aspect Ratio Of The Window
-	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
-
-	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
-	glLoadIdentity();									// Reset The Modelview Matrix
+	updateView(wHeight, wWidth);
 }
 
 void idle()
@@ -43,58 +84,94 @@ void idle()
 	glutPostRedisplay();
 }
 
-void redraw()
+
+
+
+void getFPS()
 {
-	// If want display in wireframe mode
-	//	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	static int frame = 0, time, timebase = 0;
+	static char buffer[256];
 
-	glClear(GL_COLOR_BUFFER_BIT);
-	glLoadIdentity();									// Reset The Current Modelview Matrix
+	char mode[64];
+	strcpy_s(mode, "naive");
 
-	glPushMatrix();
-	glTranslatef(-2.0f, 0.0f, -6.0f);		// Place the triangle Left
-	glTranslatef(0.0f, fTranslate, 0.0f);	// Translate in Y direction
-	Draw_Triangle();						// Draw triangle					
-	glPopMatrix();
+	frame++;
+	time = glutGet(GLUT_ELAPSED_TIME);
+	if (time - timebase > 1000) {
+		sprintf_s(buffer, "FPS:%4.2f %s",
+			frame*1000.0 / (time - timebase), mode);
+		timebase = time;
+		frame = 0;
+	}
 
-	glPushMatrix();
-	glTranslatef(0.0f, 0.0f, -6.0f);			// Place the triangle at Center
-	glRotatef(fRotate, 0, 1.0f, 0);			// Rotate around Y axis
-	Draw_Triangle();						// Draw triangle
-	glPopMatrix();
-
-
-	glPushMatrix();
-	glTranslatef(2.0f, 0.0f, -6.0f);			// Place the triangle Right
-	glScalef(fScale, fScale, fScale);		// Scale with the same value in x,y,z direction
-	Draw_Triangle();						// Draw triangle						
-	glPopMatrix();
-
-
-	fTranslate += 0.005f;
-	fRotate += 0.5f;
-	fScale -= 0.005f;
-
-	if (fTranslate > 0.5f) fTranslate = 0.0f;
-	if (fScale < 0.5f)     fScale = 1.0f;
-	glutSwapBuffers();
+	//glutSetWindowTitle(buffer);
+	char *c;
+	glDisable(GL_DEPTH_TEST);
+	glMatrixMode(GL_PROJECTION);// 选择投影矩阵
+	glPushMatrix();// 保存原矩阵
+	glLoadIdentity();// 装入单位矩阵
+	glOrtho(0, 480, 0, 480, -1, 1);// 位置正投影
+	glMatrixMode(GL_MODELVIEW);// 选择Modelview矩阵
+	glPushMatrix();// 保存原矩阵
+	glLoadIdentity();// 装入单位矩阵*/
+	glRasterPos2f(10, 10);
+	for (c = buffer; *c != '\0'; c++) {
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+	}
+	glMatrixMode(GL_PROJECTION);// 选择投影矩阵
+	glPopMatrix();// 重置为原保存矩阵
+	glMatrixMode(GL_MODELVIEW);// 选择Modelview矩阵
+	glPopMatrix();// 重置为原保存矩阵
+	glEnable(GL_DEPTH_TEST);
 }
 
+void redraw()
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();									// Reset The Current Modelview Matrix
+
+	m_Camera.setLook();			// 场景（0，0，0）的视点中心 (0,5,50)，Y轴向上
+
+	
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	
+
+
+
+	//	glTranslatef(0.0f, 0.0f,-6.0f);			// Place the triangle at Center
+	//glRotatef(fRotate, 0, 1.0f, 0);			// Rotate around Y axis
+	glRotatef(-90, 1, 0, 0);
+	glScalef(0.2, 0.2, 0.2);
+
+
+
+										//	Gen3DObjectList();
+	m_SkyBox.render();
+
+	if (bAnim) fRotate += 0.5f;
+
+	getFPS();
+
+	glutSwapBuffers();
+}
+void mouse() {
+	m_Camera.setViewByMouse();
+}
 int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(640, 480);                                                     ///////////////////////////////////////
-	int windowHandle
-		= glutCreateWindow("Simple GLUT App");
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(480, 480);
+	int windowHandle = glutCreateWindow("Simple GLUT App");
 
+	init();
 	glutDisplayFunc(redraw);
-	glutReshapeFunc(reshape);			///////////////////////////////////////
-	glutIdleFunc(idle);							///////////////////////////////////////
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(key);
+	glutIdleFunc(idle);
+
 
 	glutMainLoop();
-
 	return 0;
 }
-
-
